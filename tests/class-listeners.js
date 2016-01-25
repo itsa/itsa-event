07802d3dc4ca1a1728cfd18e7045726e1e66bbe1
Marco Asbreuk <marco@itsasbreuk.nl>
2016-01-25 08:49:41 +0100

    update dev-dependencies
    for making tests succeed

--- tests/class-listeners.js
+++ tests/class-listeners.js
@@ -5,7 +5,7 @@
 var expect = require('chai').expect,
     should = require('chai').should(),
     Event = require("../index.js"),
-    Classes = require("js-ext/extra/classes.js");
+    Classes = require("itsa-classes");
 
 describe('Classes automatic Event-listeners', function () {
     // Code to execute before every test.
